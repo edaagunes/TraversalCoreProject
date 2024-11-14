9ecb4ec4ac98c83d1ba6aca8c0f4d18d393bc59e
edaagunes <edagunes00@hotmail.com>
2024-11-14 16:29:02 +0300

    Added new Reservation Add page and transactions

--- TraversalCoreProject/Areas/Member/Controllers/ReservationController.cs
+++ TraversalCoreProject/Areas/Member/Controllers/ReservationController.cs
@@ -1,11 +1,18 @@
-﻿using EntityLayer.Concrete;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace TraversalCoreProject.Areas.Member.Controllers
 {
+    [Area("Member")]
     public class ReservationController : Controller
     {
-        [Area("Member")]
+        DestinationManager destinationManager = new DestinationManager(new EfDestinationDal());
+        ReservationManager reservationManager = new ReservationManager(new EfReservationDal());
         public IActionResult MyCurrentReservation()
         {
             return View();
@@ -18,12 +25,21 @@ namespace TraversalCoreProject.Areas.Member.Controllers
         [HttpGet]
         public IActionResult NewReservation()
         {
+            List<SelectListItem> values = (from x in destinationManager.TGetList()
+                                           select new SelectListItem
+                                           {
+                                               Text = x.City,
+                                               Value = x.DestinationId.ToString()
+                                           }).ToList();
+            ViewBag.v = values;
             return View();
         }
         [HttpPost]
         public IActionResult NewReservation(Reservation reservation)
         {
-            return View();
+            reservation.AppUserId = 1;
+            reservationManager.TAdd(reservation);
+            return RedirectToAction("MyCurrentReservation");
         }
     }
 }
