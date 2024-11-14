1dd8c3450ae5081582227f9965ade9e661063ac6
edaagunes <edagunes00@hotmail.com>
2024-11-14 15:58:47 +0300

    Added Reservation table

---
+++ TraversalCoreProject/Areas/Member/Controllers/ReservationController.cs
@@ -0,0 +1,29 @@
+﻿using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProject.Areas.Member.Controllers
+{
+    public class ReservationController : Controller
+    {
+        [Area("Member")]
+        public IActionResult MyCurrentReservation()
+        {
+            return View();
+        }
+        public IActionResult MyOldReservation()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult NewReservation()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult NewReservation(Reservation reservation)
+        {
+            return View();
+        }
+    }
+}
