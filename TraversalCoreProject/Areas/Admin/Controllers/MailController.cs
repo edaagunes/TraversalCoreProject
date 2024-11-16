fcaf4e7a0c98698088b39f6b548567babba464f0
edaagunes <edagunes00@hotmail.com>
2024-11-16 22:24:28 +0300

    Added MailController in Admin

---
+++ TraversalCoreProject/Areas/Admin/Controllers/MailController.cs
@@ -0,0 +1,54 @@
+﻿using MailKit.Net.Smtp;
+using Microsoft.AspNetCore.Mvc;
+using MimeKit;
+using System;
+using TraversalCoreProject.Models;
+
+namespace TraversalCoreProject.Areas.Admin.Controllers
+{
+	[Area("Admin")]
+	[Route("Admin/[controller]/[action]/{id?}")]
+	public class MailController : Controller
+	{
+		[HttpGet]
+		public IActionResult Index()
+		{
+			return View();
+		}
+		[HttpPost]
+		public IActionResult Index(MailRequest mailRequest)
+		{
+			try
+			{
+				MimeMessage mimeMessage = new MimeMessage();
+
+				MailboxAddress mailboxAddressFrom = new MailboxAddress("Admin", "traversalcoreeda@gmail.com");
+				mimeMessage.From.Add(mailboxAddressFrom);
+
+				MailboxAddress mailboxAddressTo = new MailboxAddress("User", mailRequest.ReceiverMail);
+				mimeMessage.To.Add(mailboxAddressTo);
+
+				var bodyBuilder = new BodyBuilder();
+				bodyBuilder.TextBody = mailRequest.Body;
+				mimeMessage.Body = bodyBuilder.ToMessageBody();
+
+				mimeMessage.Subject = mailRequest.Subject;
+
+				SmtpClient client = new SmtpClient();
+				client.Connect("smtp.gmail.com", 587, false);
+				client.Authenticate("traversalcoreeda@gmail.com", "ktae jpiv ywkt fcof");
+				client.Send(mimeMessage);
+				client.Disconnect(true);
+
+				TempData["SuccessMessage"] = "Mail başarılı bir şekilde gönderildi.";
+			}
+			catch (Exception)
+			{
+				TempData["ErrorMessage"] = "Mail gönderimi sırasında bir hata oluştu.";
+			}
+			
+
+			return View();
+		}
+	}
+}
