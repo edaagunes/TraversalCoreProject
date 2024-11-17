fc6316ba6c84add9b3db72aa822f023f001b7851
edaagunes <edagunes00@hotmail.com>
2024-11-17 19:56:34 +0300

    Added ApiMovie and consume imdb top 100 films

---
+++ TraversalCoreProject/Areas/Admin/Controllers/ApiMovieController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using System;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using TraversalCoreProject.Areas.Admin.Models;
+using Newtonsoft.Json;
+
+namespace TraversalCoreProject.Areas.Admin.Controllers
+{
+	[AllowAnonymous]
+	[Area("Admin")]
+	[Route("Admin/[controller]/[action]/{id?}")]
+	public class ApiMovieController : Controller
+	{
+		public async Task<IActionResult> Index()
+		{
+			List<ApiMovieViewModel> apiMovies = new List<ApiMovieViewModel>();
+
+			var client = new HttpClient();
+			var request = new HttpRequestMessage
+			{
+				Method = HttpMethod.Get,
+				RequestUri = new Uri("https://imdb-top-100-movies.p.rapidapi.com/"),
+				Headers =
+	{
+		{ "x-rapidapi-key", "8189ffcc04msh89957af40346776p12f8ebjsndc8eef993d4e" },
+		{ "x-rapidapi-host", "imdb-top-100-movies.p.rapidapi.com" },
+	},
+			};
+			using (var response = await client.SendAsync(request))
+			{
+				response.EnsureSuccessStatusCode();
+				var body = await response.Content.ReadAsStringAsync();
+				apiMovies=JsonConvert.DeserializeObject<List<ApiMovieViewModel>>(body);
+				return View(apiMovies);
+			}
+		}
+	}
+}
