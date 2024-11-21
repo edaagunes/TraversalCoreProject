50e394b697be17d6f59340c57edeebad5393b1f5
edaagunes <edagunes00@hotmail.com>
2024-11-22 01:37:49 +0300

    Added ContactUs form

--- BusinessLayer/Container/Extensions.cs
+++ BusinessLayer/Container/Extensions.cs
@@ -3,10 +3,12 @@ using BusinessLayer.Abstract.AbstractUow;
 using BusinessLayer.Concrete;
 using BusinessLayer.Concrete.ConcreteUow;
 using BusinessLayer.ValidationRules;
+using BusinessLayer.ValidationRules.ContactUsValidationRules;
 using DataAccessLayer.Abstract;
 using DataAccessLayer.EntityFramework;
 using DataAccessLayer.UnitOfWork;
 using DTOLayer.DTOs.AnnouncementDTOs;
+using DTOLayer.DTOs.ContactDTOs;
 using FluentValidation;
 using Microsoft.Extensions.DependencyInjection;
 using System;
@@ -56,6 +58,7 @@ namespace BusinessLayer.Container
 		public static void CustomValidator(this IServiceCollection services)
 		{
 			services.AddTransient<IValidator<AnnouncementAddDto>, AnnouncementValidator>();
+			services.AddTransient<IValidator<SendMessageDto>, SendContactUsValidator>();
 		}
 	}
 }
