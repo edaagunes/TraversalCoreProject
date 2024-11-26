51f913f79cdf6ef60a0e847aa84d9aaf19932417
edaagunes <edagunes00@hotmail.com>
2024-11-26 04:14:32 +0300

    Completed UI panel

---
+++ TraversalCoreProject/Controllers/AboutController.cs
@@ -0,0 +1,22 @@
+﻿using BusinessLayer.Abstract;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace TraversalCoreProject.Controllers
+{
+	public class AboutController : Controller
+	{
+		private readonly IGuideService _guideService;
+
+		public AboutController(IGuideService guideService)
+		{
+			_guideService = guideService;
+		}
+
+		public IActionResult Index()
+		{
+			var values=_guideService.TGetList().Take(4);
+			return View(values);
+		}
+	}
+}
